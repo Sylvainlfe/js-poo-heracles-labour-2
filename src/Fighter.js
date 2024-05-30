6fc0af8926ea5d0954f29dc79523c4b9a82fab7b
sylvain.lafaye <sylvain-lafaye@hotmail.fr>
2024-05-30 12:20:21 +0200

    first commit

--- src/Fighter.js
+++ src/Fighter.js
@@ -1,19 +1,21 @@
 const MAX_LIFE = 100;
 
 class Fighter {
-    constructor(name, strength, dexterity) {
+    constructor(name, strength, dexterity, weapon = null, shield = null) {
         this.name = name;
         this.strength = strength;
         this.dexterity = dexterity;
         this.life = MAX_LIFE;
+        this.weapon = weapon
+        this.shield = shield
     }
 
 
     // Launch a fight
     fight(defender) {
-        const attackPoints = this.getRandomInt(this.strength);
+        const attackPoints = this.getRandomInt(this.getDamage());
 
-        const damages = Math.max(attackPoints - defender.dexterity, 0);
+        const damages = Math.max(attackPoints - defender.getDefense(), 0);
 
         defender.life = Math.max(defender.life - damages, 0);
     }
@@ -23,6 +25,22 @@ class Fighter {
     getRandomInt(max) {
         return 1 + Math.floor(Math.random() * max);
     }
+
+    getDamage(){
+        if (this.weapon !== null) {
+            return this.strength + this.weapon.damage;
+        } else {
+            return this.strength;
+        }
+    };
+
+    getDefense(){
+        if (this.shield !== null) {
+            return this.dexterity + this.shield.protection;
+        } else {
+            return this.dexterity;
+        }
+    };
 
 
     // Determine if a fighter is still alive
